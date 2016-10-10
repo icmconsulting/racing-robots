225f68d48e144381a835bcb80ecb3e65083b8926
Brendan Bates <batesbrendan@gmail.com>
2016-10-10 23:50:44 +1100

    Resize board to fit properly in viewport

--- src/cljs/rr/board_browser.cljs
+++ src/cljs/rr/board_browser.cljs
@@ -2,12 +2,26 @@
   (:require [rr.boards :as boards]
             [rr.game :as game]
             [rr.bs :refer [list-group list-group-item]]
-            [reagent.core :refer [atom]]
+            [reagent.core :refer [atom dom-node]]
             [rr.konva :as k]))
 
 ;;TODO: tweak these
-(def board-width 1000)
-(def board-height 450)
+
+(def board-size (atom {:width 1000 :height 450}))
+
+(defn calculate-optimal-board-size
+  [viewport-size board]
+  (let [[cols rows] (game/board-size board)]
+    (if (<= cols rows)
+      (let [optimal-height (- (:height viewport-size) 20)
+            row-width (/ optimal-height rows)]
+        {:width  (* cols row-width)
+         :height optimal-height})
+
+      (let [optimal-width (- (:width viewport-size) 20)
+            row-height (/ optimal-width cols)]
+        {:width  optimal-width
+         :height (* rows row-height)}))))
 
 (def base-square-image
   (let [obj (js/Image.)]
@@ -27,27 +41,27 @@
 
 (defn board-row
   [height y row]
-  (let [square-width (/ board-width (count row))]
-    ^{:key (str y)}
-    [k/group
-     (map-indexed
-       (fn [idx square]
-         (let [x (* idx square-width)]
-           ^{:key (str idx "-" y)}
-           [k/group
-            [k/image {:height height
-                      :width  square-width
-                      :image  base-square-image
-                      :x      x
-                      :y      y}]
-            [k/rect {:x x :y y
-                     :height height :width square-width
-                     :stroke "#ffffff" :stroke-width 1}]]))
-       row)]))
+  ^{:key (str y)}
+  [k/group
+   (map-indexed
+     (fn [idx square]
+       (let [x (* idx height)]
+         ^{:key (str idx "-" y)}
+         [k/group
+          [k/image {:height height
+                    :width  height
+                    :image  base-square-image
+                    :x      x
+                    :y      y}]
+          [k/rect {:x      x :y y
+                   :height height :width height
+                   :stroke "#ffffff" :stroke-width 1}]]))
+     row)])
 
 (defn board-view
   [board]
   (let [rows (game/rows board)
+        {board-width :width board-height :height} (calculate-optimal-board-size @board-size board)
         row-height (/ board-height (count rows))]
     [k/stage {:ref "board" :width board-width :height board-height :container "board-section"}
      [k/layer
@@ -62,6 +76,26 @@
     [board-view (get (second @selected-board) :board)]
     [:div.no-board-selected [:p "Select a board to view from the board list on the left."]]))
 
+(defn middle-section*
+  [selected-board]
+  [:section#board-section.middle [selected-board-view selected-board]])
+
+(let [prev-to (atom nil)]
+  (defn on-window-resize [target-node ev]
+    (when-let [to @prev-to] (.clearTimeout js/window to))
+
+    (reset! prev-to (.setTimeout js/window
+                                 (fn []
+                                   (reset! board-size {:width (.-clientWidth target-node)
+                                                       :height (.-clientHeight target-node)}))
+                                 500))))
+
+(def middle-section
+  (with-meta middle-section*
+             {:component-did-mount #(.addEventListener js/window "resize" (partial on-window-resize (dom-node %)))
+              :component-will-unmount #(.removeEventListener js/window "resize" (partial on-window-resize (dom-node %)))}))
+
+
 (defn board-info
   []
   )
@@ -72,5 +106,6 @@
     (fn [_]
       [:section.board-browser-root
        [:section.left [board-list selected-board]]
-       [:section#board-section.middle [selected-board-view selected-board]]
+       [middle-section selected-board]
        [:section.right [board-info]]])))
+
